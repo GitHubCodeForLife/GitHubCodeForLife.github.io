eb9583227f93937d22f2e5e06ad733b1fe35142b
Tran Tu <65528999+GitHubCodeForLife@users.noreply.github.com>
2025-03-09 16:00:08 +0700

    update

--- canvas/webpack.config.js
+++ canvas/webpack.config.js
@@ -18,8 +18,14 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
       },
     ],
   },
